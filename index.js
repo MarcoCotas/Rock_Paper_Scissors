e20449ecc259ad8954e6ebb2fe77f04d73f36c6b
MarcoCotas <marco.cotas06@gmail.com>
2025-03-25 19:24:26 +0000

    Add selectors and button click function

--- index.js
+++ index.js
@@ -1,68 +1,39 @@
-/*
-Create a function getHumanChoice to return the user input value (Rock, Paper, Scissors)
-Get computer random choice (Math.random)
-Compare the two choices and determine the winner
-Track results and display the winner in variables humanScore and computerScore
-*/
-
-//Wanna checkout branch
-
 function getComputerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
   return choices[Math.floor(Math.random() * 3)];
 }
 
-function getHumanChoice() {
-  let human;
-  human = prompt("Choose wisely: Rock - Paper - Scissors");
-
-  return human.charAt(0).toUpperCase() + human.slice(1).toLowerCase();
-}
-
 function playRound(cpu, men) {
   if (men === cpu) {
     console.log("It's a tie!");
-    return [0, 0]; // Tie
   } else if (
     (men === "Rock" && cpu === "Scissors") ||
     (men === "Scissors" && cpu === "Paper") ||
     (men === "Paper" && cpu === "Rock")
   ) {
     console.log(`You win! ${men} beats ${cpu}`);
-    return [1, 0]; // Human Victory
   } else {
     console.log(`You lose! ${cpu} beats ${men}`);
-    return [0, 1]; // CPU Victory
-  }
-}
-
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-
-  for (let i = 0; i < 5; i++) {
-    const cpu = getComputerChoice();
-    const men = getHumanChoice();
-    console.log(`Round ${i + 1}: You chose ${men}, Computer chose ${cpu}`);
-
-    let [hScore, cScore] = playRound(cpu, men);
-    humanScore += hScore;
-    computerScore += cScore;
-
-    console.log(
-      `Current Score -> You: ${humanScore} | Computer: ${computerScore}`
-    );
-  }
-
-  // Final Winner
-  if (humanScore > computerScore) {
-    console.log("🎉 Congrats, you win the game!");
-  } else if (humanScore < computerScore) {
-    console.log("💀 You lost the game. Better luck next time!");
-  } else {
-    console.log("🤝 It's a tie overall!");
   }
 }
 
 // Start the game
-playGame();
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const cpu = getComputerChoice();
+
+    const men =
+      button.id.charAt(0).toUpperCase() + button.id.slice(1).toLowerCase();
+    alert(`You choose ${men} and CPU choose ${cpu}`);
+    playRound(cpu, men);
+  });
+});
+
+const game = document.querySelector("game");
+const currentResult = document.querySelector("current-result");
+const humanChoice = document.querySelector("human-choice");
+const computerChoice = document.querySelector("computer-choice");
+const roundResult = document.querySelector("roundResult");
+const finalScore = document.querySelector("final-score");
